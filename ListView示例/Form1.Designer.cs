4fe6b76b30833eea7f4b797097adcf866df104d5
StupidGao <304239453@qq.com>
2020-02-13 22:26:57 +0800

    'first_commit'

---
+++ ListView示例/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace ListView示例
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lv = new System.Windows.Forms.ListView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnTest1 = new System.Windows.Forms.Button();
+            this.tbxAdd = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbxPhone = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbbSex = new System.Windows.Forms.ComboBox();
+            this.tbxName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lv
+            // 
+            this.lv.Activation = System.Windows.Forms.ItemActivation.OneClick;
+            this.lv.FullRowSelect = true;
+            this.lv.GridLines = true;
+            this.lv.LabelEdit = true;
+            this.lv.Location = new System.Drawing.Point(6, 24);
+            this.lv.Name = "lv";
+            this.lv.Size = new System.Drawing.Size(530, 337);
+            this.lv.TabIndex = 2;
+            this.lv.UseCompatibleStateImageBehavior = false;
+            this.lv.SelectedIndexChanged += new System.EventHandler(this.lv_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lv);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(542, 367);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "listview列表信息";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.btnTest1);
+            this.groupBox2.Controls.Add(this.tbxAdd);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.tbxPhone);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.cbbSex);
+            this.groupBox2.Controls.Add(this.tbxName);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(18, 386);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(307, 176);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "各项操作";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(199, 135);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(102, 35);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "删除全部";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(199, 95);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(102, 30);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "删除选定行";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(199, 54);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 28);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "修改";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnTest1
+            // 
+            this.btnTest1.ForeColor = System.Drawing.Color.Black;
+            this.btnTest1.Location = new System.Drawing.Point(199, 17);
+            this.btnTest1.Name = "btnTest1";
+            this.btnTest1.Size = new System.Drawing.Size(102, 31);
+            this.btnTest1.TabIndex = 8;
+            this.btnTest1.Text = "新增";
+            this.btnTest1.UseVisualStyleBackColor = true;
+            this.btnTest1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbxAdd
+            // 
+            this.tbxAdd.Location = new System.Drawing.Point(54, 135);
+            this.tbxAdd.Name = "tbxAdd";
+            this.tbxAdd.Size = new System.Drawing.Size(122, 25);
+            this.tbxAdd.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "籍贯";
+            // 
+            // tbxPhone
+            // 
+            this.tbxPhone.Location = new System.Drawing.Point(54, 95);
+            this.tbxPhone.Name = "tbxPhone";
+            this.tbxPhone.Size = new System.Drawing.Size(100, 25);
+            this.tbxPhone.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "电话";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "性别";
+            // 
+            // cbbSex
+            // 
+            this.cbbSex.FormattingEnabled = true;
+            this.cbbSex.Items.AddRange(new object[] {
+            "男",
+            "女"});
+            this.cbbSex.Location = new System.Drawing.Point(54, 59);
+            this.cbbSex.Name = "cbbSex";
+            this.cbbSex.Size = new System.Drawing.Size(71, 23);
+            this.cbbSex.TabIndex = 2;
+            this.cbbSex.Text = "男";
+            this.cbbSex.SelectedIndexChanged += new System.EventHandler(this.cbbSex_SelectedIndexChanged);
+            // 
+            // tbxName
+            // 
+            this.tbxName.Location = new System.Drawing.Point(54, 21);
+            this.tbxName.Name = "tbxName";
+            this.tbxName.Size = new System.Drawing.Size(126, 25);
+            this.tbxName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "姓名";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Controls.Add(this.textBox1);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Location = new System.Drawing.Point(331, 386);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(217, 176);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "查询";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(56, 33);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 15);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "是否存在姓名";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(186, 25);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(59, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "查询";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 568);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "ListView示例";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lv;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnTest1;
+        private System.Windows.Forms.TextBox tbxAdd;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbxPhone;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbbSex;
+        private System.Windows.Forms.TextBox tbxName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ ListView示例/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListView示例
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.lv.View = View.Details;
+            lv.Columns.Add("ID", 40, HorizontalAlignment.Center);
+            lv.Columns.Add("姓名", 60,HorizontalAlignment.Center);
+            lv.Columns.Add("性别", 40, HorizontalAlignment.Center);
+            lv.Columns.Add("电话", 100, HorizontalAlignment.Center);
+            lv.Columns.Add("籍贯", 50, HorizontalAlignment.Center);
+
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //定义新Item变量
+            ListViewItem i_item = lv.Items.Add((lv.Items.Count + 1) + "");//这个特别好，实现功能值得学习
+            i_item.SubItems.Add(tbxName.Text); //添加姓名 
+            i_item.SubItems.Add(cbbSex.Text);  //添加性别
+            i_item.SubItems.Add(tbxPhone.Text);
+            i_item.SubItems.Add(tbxAdd.Text);
+            i_item.EnsureVisible();
+        }
+
+        private void lv_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lv.SelectedItems.Count > 0)
+            {
+                tbxName.Text = lv.SelectedItems[0].SubItems[1].Text;
+                cbbSex.Text = lv.SelectedItems[0].SubItems[2].Text;
+                tbxPhone.Text = lv.SelectedItems[0].SubItems[3].Text;
+                tbxAdd.Text = lv.SelectedItems[0].SubItems[4].Text;
+            }
+        }
+
+        private void cbbSex_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+        //修改选定行数据
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (lv.SelectedItems.Count > 0)
+            {
+                lv.SelectedItems[0].SubItems[1].Text = tbxName.Text;
+                lv.SelectedItems[0].SubItems[2].Text = cbbSex.Text;
+                lv.SelectedItems[0].SubItems[3].Text = tbxPhone.Text;
+                lv.SelectedItems[0].SubItems[4].Text = tbxAdd.Text;
+            }
+        }
+        //删除某选定行数据
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (lv.SelectedItems.Count > 0) //如果选择了某行数据
+            {
+                lv.SelectedItems[0].Remove();//删除选定行
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            lv.Items.Clear();
+        }
+        //查询所有行数据
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (lv.Items.Count > 0) //列表有数据
+            {
+                foreach (ListViewItem lt in lv.Items) //轮询列表数据
+                {
+                    if (lt.SubItems[1].Text == textBox1.Text)
+                    {
+                        MessageBox.Show("存在该姓名");
+                        return;
+                    }
+                }
+                MessageBox.Show("未找到技能");
+            }
+            else//列表数据为空
+                MessageBox.Show("为输入列表数据");
+        }
+    }
+}
